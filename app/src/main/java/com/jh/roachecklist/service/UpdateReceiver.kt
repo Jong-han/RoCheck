b139df015fe57bc38cacf375f831ca3bc166dd37
jh <tltty123@naver.com>
2021-08-21 10:13:20 +0900

    업데이트 시, 알람 재적용 하는 브로드캐스트 리시버 등록

--- app/src/main/java/com/jh/roachecklist/service/UpdateReceiver.kt
+++ app/src/main/java/com/jh/roachecklist/service/UpdateReceiver.kt
@@ -1,59 +1,40 @@
-package com.jh.roachecklist
+package com.jh.roachecklist.service
 
 import android.app.AlarmManager
-import android.app.Application
 import android.app.PendingIntent
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+import com.jh.roachecklist.Const
 import com.jh.roachecklist.preference.AppPreference
-import com.jh.roachecklist.repository.Repository
-import com.jh.roachecklist.service.AlarmReceiver
-import com.jh.roachecklist.service.RefreshReceiver
 import com.jh.roachecklist.utils.DefaultNotification
-import dagger.hilt.android.HiltAndroidApp
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import dagger.hilt.android.AndroidEntryPoint
 import java.util.*
 import javax.inject.Inject
 
-@HiltAndroidApp
-class App: Application() {
+@AndroidEntryPoint
+class UpdateReceiver : BroadcastReceiver() {
 
-    @Inject
-    lateinit var pref: AppPreference
-    @Inject
-    lateinit var repository: Repository
+    @Inject lateinit var pref: AppPreference
 
-    override fun onCreate() {
+    override fun onReceive(context: Context, intent: Intent) {
 
-        super.onCreate()
+        if (intent.action.equals(Intent.ACTION_MY_PACKAGE_REPLACED)) {
 
-//        val br = UpdateReceiver()
-//        val intentFilter = IntentFilter().apply {
-//            addAction(Intent.ACTION_MY_PACKAGE_REPLACED)
-//        }
-//        registerReceiver( br, intentFilter )
+            pref.getPref()
 
-        pref.getPref()
-        if ( pref.isFirst ) {
+            val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
-            CoroutineScope( Dispatchers.IO).launch {
-
-                repository.insertCheckList()
-                pref.isFirst = false
-
-            }
-            val alarmManager = applicationContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-
-            val alarmIntent = Intent( applicationContext, AlarmReceiver::class.java )
+            val alarmIntent = Intent( context, AlarmReceiver::class.java )
             val alarmPendingIntent = PendingIntent.getBroadcast(
-                applicationContext, DefaultNotification.NOTIFICATION_CODE_DEFAULT, alarmIntent,
+                context, DefaultNotification.NOTIFICATION_CODE_DEFAULT, alarmIntent,
                 PendingIntent.FLAG_UPDATE_CURRENT)
 
             val alarmCalendar = Calendar.getInstance()
-            alarmCalendar.set(Calendar.HOUR_OF_DAY, 18 )
-            alarmCalendar.set(Calendar.MINUTE, 0 )
+            alarmCalendar.set(Calendar.HOUR_OF_DAY, pref.hour )
+            alarmCalendar.set(Calendar.MINUTE, pref.minute )
             alarmCalendar.set(Calendar.SECOND, 0)
             alarmCalendar.set(Calendar.MILLISECOND, 0)
 
@@ -64,9 +45,9 @@ class App: Application() {
 
             alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, alarmTriggerTime, AlarmManager.INTERVAL_DAY, alarmPendingIntent)
 
-            val refreshIntent = Intent( applicationContext, RefreshReceiver::class.java )
+            val refreshIntent = Intent( context, RefreshReceiver::class.java )
             val refreshPendingIntent = PendingIntent.getBroadcast(
-                applicationContext, DefaultNotification.NOTIFICATION_CODE_DEFAULT, refreshIntent,
+                context, DefaultNotification.NOTIFICATION_CODE_DEFAULT, refreshIntent,
                 PendingIntent.FLAG_UPDATE_CURRENT)
 
             val refreshCalendar = Calendar.getInstance()
